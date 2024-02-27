fdbdb29607033f742425deb47358f3ebd2e3dbd2
AmanRajVerma10 <verma.aman464@gmail.com>
2024-02-28 03:09:19 +0530

    s

--- forgotPassword.js
+++ forgotPassword.js
@@ -3,7 +3,7 @@ const form= document.getElementById('forgotPasswordForm')
 form.addEventListener("submit",async(e)=>{
     e.preventDefault();
     const email=e.target.email.value;
-    const response= await axios.post('http://localhost:300/password/forgotpassword',{
+    const response= await axios.post('http://localhost:3000/password/forgotpassword',{
         email
     })
     console.log(response);
